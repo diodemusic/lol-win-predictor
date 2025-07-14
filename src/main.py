9c6d57706938c0f9b2f646fa7514a6b987e6d3d9
saves <diodemusicuk@gmail.com>
2025-07-14 03:56:04 +0100

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,2 @@
-import riot_api
-
 if __name__ == "__main__":
-    riot = riot_api.Riot()
-    puuid = riot.get_puuid(riot_api.Continent.americas, "McDonaldsManager", "mid")
-    match_ids = riot.get_match_ids(riot_api.Continent.americas, puuid)
-    match = riot.get_match(riot_api.Continent.americas, match_ids[0])
-    print(match)
+    print("hello world")
