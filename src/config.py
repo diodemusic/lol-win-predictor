31bed45ed955c24a47b1b1a4893f03e01e3d20c4
saves <diodemusicuk@gmail.com>
2025-07-14 02:23:16 +0100

    Add Config class

---
+++ src/config.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+import os
+from sys import exit
+from enum import Enum
+
+
+class EnvVarNotFound(Exception):
+    def __str__(self):
+        return "Enviroment variable not found."
+
+
+class _keys(Enum):
+    # discord_token = "DISCORD_TOKEN"
+    riot_api_key = "RIOT_API_KEY"
+
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+
+    def _get_secret(self, secret):
+        SECRET = os.getenv(secret)
+
+        if not SECRET:
+            raise EnvVarNotFound
+
+        return SECRET
+
+    # def get_discord_token(self):
+    #     secret = _keys.discord_token.value
+
+    #     try:
+    #         return self._get_token(secret)
+    #     except EnvVarNotFound as e:
+    #         print(f"Error getting {secret}: {e}")
+    #         exit(1)
+
+    def get_riot_api_key(self):
+        secret = _keys.riot_api_key.value
+
+        try:
+            return self._get_secret(secret)
+        except EnvVarNotFound as e:
+            print(f"Error getting {secret}: {e}")
+            exit(1)
+
+
+config = Config()
+print(config.get_riot_api_key())
