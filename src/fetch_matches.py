73a7677fdbda302f1305b71c39da184cc76e3822
saves <diodemusicuk@gmail.com>
2025-07-14 17:18:39 +0100

    Refactor script into function

--- src/fetch_matches.py
+++ src/fetch_matches.py
@@ -1,9 +1,22 @@
 import riot_api
 
+import json
+
 riot = riot_api.Riot()
 
-puuid = riot.get_puuid(riot_api.Continent.americas, "McDonaldsManager", "mid")
-match_ids = riot.get_match_ids(riot_api.Continent.americas, puuid)
-match = riot.get_match(riot_api.Continent.americas, match_ids[0])
 
-print(match)
+def fetch_matches(
+    continent: riot_api.Continent,
+    game_name: str,
+    tag_line: str,
+    count: int = 100,
+    path: str = "../data/raw",
+):
+    puuid = riot.get_puuid(continent, game_name, tag_line)
+    match_ids = riot.get_match_ids(continent, puuid, count=count)
+
+    for match_id in match_ids:
+        match = riot.get_match(continent, match_id)
+
+        with open(f"{path}/{match_id}.json", "w", encoding="utf-8") as f:
+            json.dump(match, f)
